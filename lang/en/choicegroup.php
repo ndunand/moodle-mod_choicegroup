76bc2927274415c59b3044be428c3f4146cb0579
Mart Mangus <mart@mangus.ee>
2013-05-02 12:25:52 +0300

    Possibility to show/hide group description in group choice view.

--- lang/en/choicegroup.php
+++ lang/en/choicegroup.php
@@ -103,3 +103,6 @@ $string['members/max'] = 'Fill / Capacity';
 $string['members/'] = 'Fill';
 $string['groupmembers'] = 'Group members';
 $string['page-mod-choice-x'] = 'Any Group choice module page';
+$string['showdescription'] = 'Show descriptions';
+$string['hidedescription'] = 'Hide descriptions';
+
