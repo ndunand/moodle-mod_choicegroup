10baf6ff90af4fb711f6bdfefa33db2306d52389
Thomas Korner <korner@let.ethz.ch>
2014-05-21 15:03:14 +0200

    add DE lang for new setup window
    added additional German strings for the new setup window. All of the
    standard strings are still with AMOS.

---
+++ lang/de/choicegroup.php
@@ -0,0 +1,47 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'choice', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   choice
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['groupsheader'] = "Gruppen";
+$string['the_value_you_entered_is_not_a_number'] = "Der eingegebene Wert ist keine Zahl.";
+$string['add_groupings'] = "Gruppierungen hinzufügen";
+$string['add_grouping'] = "Gruppierung hinzufügen";
+$string['add_groups'] = "Gruppen hinzufügen";
+$string['del_groups'] = "Gruppen entfernen";
+$string['del_group'] = "Gruppe entfernen";
+$string['add_group'] = "Gruppe hinzufügen";
+$string['add'] = "Hinzufügen";
+$string['del'] = "Entfernen";
+$string['set_limit_for_group'] = "Limite für ";
+$string['available_groups'] = 'Verfügbare Gruppen';
+$string['selected_groups'] = 'Ausgewählte Gruppen';
+$string['char_bullet_collapsed'] = '►';
+$string['char_bullet_expanded'] = '▼';
+$string['char_limitui_parenthesis_start'] = '⦗';
+$string['char_limitui_parenthesis_end'] = '⦘';
+$string['expand_all_groupings'] = 'Alle Gruppierungen aufklappen';
+$string['collapse_all_groupings'] = 'Alle Gruppierungen einklappen';
+$string['double_click_grouping_legend'] = 'Zum individuellen Auf-/Einklappen auf eine Gruppierung doppelklicken.';
+$string['double_click_group_legend'] = 'Zum Hinzufügen eine Gruppe doppelklicken.';
+
