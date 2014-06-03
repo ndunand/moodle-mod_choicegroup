114d6820d85130eef8da3c0c171e315ccbe14c49
Nicolas Dunand <nico@kolya.ch>
2014-06-03 14:02:58 +0200

    Corrected version info.

--- version.php
+++ version.php
@@ -26,12 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015050100;
-$plugin->requires  = 2014050800;
+$plugin->version  = 2014060100;
+$plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.5 (Build: 2015010100)';
+$plugin->release = '1.5 (Build: 2014060100)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
-
 
