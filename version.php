703bc3b67e4f476146c32115635c0776142fb739
Nicolas Dunand <nico@kolya.ch>
2014-09-12 15:49:54 +0200

    Bumper version number

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version  = 2014072101;
 $plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.6.3 (Build: 2014072103)';
+$plugin->release = '1.6.4 (Build: 2014072104)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
