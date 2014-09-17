b0b15a37c76aaa9c63044bbbbb49707e6d2c19bd
Nicolas Dunand <nico@kolya.ch>
2014-09-17 09:55:40 +0200

    Increased version number

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014072104;
+$plugin->version  = 2014091700;
 $plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.6.4 (Build: 2014072104)';
+$plugin->release = '1.7 (Build: 2014091700)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
