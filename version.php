10f7564fb93ac53d89ce4b0bcfc99bb74290dea0
Nicolas Dunand <nico@kolya.ch>
2015-02-27 15:43:53 +0100

    Increased version number for new release on moodle.org

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015022301;
+$plugin->version  = 2015022302;
 $plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.8.1 for Moodle 2.7-2.8 (Build: 2015010101)';
+$plugin->release = '1.8.2 for Moodle 2.7-2.8 (Build: 2015022302)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
