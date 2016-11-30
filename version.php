5d1d5af90bcbe524bcf919dd9a86c46c7f00234d
Nicolas Dunand <nico@kolya.ch>
2016-11-30 12:30:18 +0100

    Official support for Moodle 3.2

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016100600;
+$plugin->version  = 2016113000;
 $plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.9.3 for Moodle 2.7-3.1 (Build: 2016100600)';
+$plugin->release = '1.9.4 for Moodle 2.7-3.2 (Build: 2016113000)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
