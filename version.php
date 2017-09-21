67420d4923035f9895252d28b45a0f45b532e1b0
Nicolas Dunand <nico@kolya.ch>
2017-09-21 13:13:40 +0200

    Bumped version number for bugfix release

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017050101;
+$plugin->version  = 2017092100;
 $plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.9.6 for Moodle 2.7-3.3 (Build: 2017050101)';
+$plugin->release = '1.9.7 for Moodle 2.7-3.3 (Build: 2017092100)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
