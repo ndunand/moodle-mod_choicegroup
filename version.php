4299010b67e105145d883bf6c8050a6ceec0d439
Nicolas Dunand <nico@kolya.ch>
2019-12-10 17:37:54 +0100

    Bumped version number

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019051001;
+$plugin->version  = 2019051002;
 $plugin->requires  = 2014050800; // Moodle 2.7
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.17 for Moodle 2.7-3.7 (Build: 2019051001)';
+$plugin->release = '1.18 for Moodle 2.7-3.8 (Build: 2019051002)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
