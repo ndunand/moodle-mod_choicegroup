4dd3bef5769220c853d9046831eb96bfcbfcc9c1
Nicolas Dunand <nico@kolya.ch>
2021-06-02 14:03:10 +0200

    Bumped version number for new release

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021060100;
+$plugin->version  = 2021060200;
 $plugin->requires  = 2018051700; // Moodle 3.5
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.31 for Moodle 3.5-3.11 (Build: 2021060100)';
+$plugin->release = '1.31 for Moodle 3.5-3.11 (Build: 2021060200)';
 
 $plugin->component = 'mod_choicegroup';
 $plugin->cron = 0;
