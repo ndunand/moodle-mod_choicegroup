266cfb24ce87a0a7ebd57441573d67001628cb17
Sameer Ahmed <itssameer@gmail.com>
2021-07-14 13:24:23 -0500

    Max Enrollments Number
    Fix: #142
    -If 'Allow enrollment to multiple groups' is selected, there is an extra field to provide max enrollments number. Use default value 0 if there is no limit.

--- db/upgrade.php
+++ db/upgrade.php
@@ -67,6 +67,21 @@ function xmldb_choicegroup_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2015022301, 'choicegroup');
     }
 
+    if ($oldversion < 2021071400) {
+
+        // Define field maxenrollments to be added to choicegroup.
+        $table = new xmldb_table('choicegroup');
+        $field = new xmldb_field('maxenrollments', XMLDB_TYPE_INTEGER, '10', null, null, null, '0', 'sortgroupsby');
+
+        // Conditionally launch add field maxenrollments.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Choicegroup savepoint reached.
+        upgrade_mod_savepoint(true, 2021071400, 'choicegroup');
+    }
+
     return true;
 }
 
--- view.php
+++ view.php
@@ -33,6 +33,7 @@ $id         = required_param('id', PARAM_INT);                 // Course Module 
 $action     = optional_param('action', '', PARAM_ALPHA);
 $userids    = optional_param_array('userid', array(), PARAM_INT); // array of attempt ids for delete action
 $notify     = optional_param('notify', '', PARAM_ALPHA);
+$maxenrollments     = optional_param('maxenrollments', '', PARAM_INT);
 
 $url = new moodle_url('/mod/choicegroup/view.php', array('id'=>$id));
 if ($action !== '') {
@@ -102,6 +103,21 @@ if (data_submitted() && is_enrolled($context, null, 'mod/choicegroup:choose') &&
 
     if ($choicegroup->multipleenrollmentspossible == 1) {
         $number_of_groups = optional_param('number_of_groups', '', PARAM_INT);
+        $enrollmentscount = 0;
+        $maxenrollments = $choicegroup->maxenrollments;
+
+        if($maxenrollments > 0){
+            for ($i = 0; $i < $number_of_groups; $i++) {
+                $answer_value = optional_param('answer_' . $i, '', PARAM_INT);
+                if ($answer_value != '') {
+                    $enrollmentscount++;
+                }
+            }
+            if ($enrollmentscount > $maxenrollments) {
+                redirect(new moodle_url('/mod/choicegroup/view.php',
+                    array('id' => $cm->id, 'notify' => 'mustchoosemax', 'sesskey' => sesskey(), 'maxenrollments' => $maxenrollments)));
+            }
+        }        
 
         for ($i = 0; $i < $number_of_groups; $i++) {
             $answer_value = optional_param('answer_' . $i, '', PARAM_INT);
@@ -163,6 +179,8 @@ if ($notify and confirm_sesskey()) {
         echo $OUTPUT->notification(get_string('choicegroupsaved', 'choicegroup'), 'notifysuccess');
     } else if ($notify === 'mustchooseone') {
         echo $OUTPUT->notification(get_string('mustchooseone', 'choicegroup'), 'notifyproblem');
+    } else if ($notify === 'mustchoosemax') {
+        echo $OUTPUT->notification(get_string('mustchoosemax', 'choicegroup', $maxenrollments), 'notifyproblem');
     }
 }
 
