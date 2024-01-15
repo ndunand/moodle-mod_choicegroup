507cf79419026a4c7d95c6f78fedbab8c23d3926
Nicolas Alexandropoulos <Nicolas.Alexandropoulos@unil.ch>
2024-01-15 17:11:23 +0100

    Improve accessibility (issue #174)

--- renderer.php
+++ renderer.php
@@ -171,7 +171,7 @@ class mod_choicegroup_renderer extends plugin_renderer_base {
                $html .= html_writer::tag('p', get_string('choicegroupfull', 'choicegroup'));
             } else {
                 if (!$disabled) {
-                    $html .= html_writer::empty_tag('input', array(
+                    $html .= html_writer::tag('button', get_string('savemychoicegroup','choicegroup'), array(
                         'type'=>'submit',
                         'value'=>get_string('savemychoicegroup','choicegroup'),
                         'class'=>'btn btn-primary',
