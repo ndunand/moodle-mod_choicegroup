bbbc8f3b303b68a63c60016ab409392595cfec59
Luca Bösch <luca.boesch@bfh.ch>
2024-07-09 11:27:14 +0200

    Fix static after visibility declaration codechecker results.

--- backup/moodle2/restore_choicegroup_activity_task.class.php
+++ backup/moodle2/restore_choicegroup_activity_task.class.php
@@ -52,7 +52,7 @@ class restore_choicegroup_activity_task extends restore_activity_task {
      * Define the contents in the activity that must be
      * processed by the link decoder
      */
-    static public function define_decode_contents() {
+    public static function define_decode_contents() {
         $contents = [];
 
         $contents[] = new restore_decode_content('choicegroup', ['intro'], 'choicegroup');
@@ -64,7 +64,7 @@ class restore_choicegroup_activity_task extends restore_activity_task {
      * Define the decoding rules for links belonging
      * to the activity to be executed by the link decoder
      */
-    static public function define_decode_rules() {
+    public static function define_decode_rules() {
         $rules = [];
 
         $rules[] = new restore_decode_rule('CHOICEGROUPVIEWBYID', '/mod/choicegroup/view.php?id=$1', 'course_module');
@@ -80,7 +80,7 @@ class restore_choicegroup_activity_task extends restore_activity_task {
      * choicegroup logs. It must return one array
      * of {@link restore_log_rule} objects
      */
-    static public function define_restore_log_rules() {
+    public static function define_restore_log_rules() {
         $rules = [];
 
         $rules[] = new restore_log_rule('choicegroup', 'add', 'view.php?id={course_module}', '{choicegroup}');
@@ -103,7 +103,7 @@ class restore_choicegroup_activity_task extends restore_activity_task {
      * by the restore final task, but are defined here at
      * activity level. All them are rules not linked to any module instance (cmid = 0)
      */
-    static public function define_restore_log_rules_for_course() {
+    public static function define_restore_log_rules_for_course() {
         $rules = [];
 
         // Fix old wrong uses (missing extension).
